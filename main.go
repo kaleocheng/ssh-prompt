eab21fa6a232bafb4a0b29b07a2ba945cef914d8
Kaleo <kaleocheng@gmail.com>
2019-03-21 23:29:53 +0800

    Add -show flag

--- main.go
+++ main.go
@@ -1,24 +1,32 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"os/exec"
 	"strings"
 
 	prompt "github.com/c-bata/go-prompt"
+	"github.com/k0kubun/pp"
 	"github.com/kaleocheng/sshconfig"
 )
 
 func main() {
+	showPtr := flag.Bool("show", false, "show ssh config")
+	flag.Parse()
+
 	c, err := sshconfig.ParseSSHConfig([]string{})
 	if err != nil {
 		fmt.Println(err)
 	}
 
+	m := make(map[string]*sshconfig.SSHHost)
+
 	var hosts []string
 	for _, item := range c {
 		hosts = append(hosts, item.Host...)
+		m[item.Host[0]] = item
 	}
 
 	h := strings.TrimSpace(prompt.Choose("host> ", hosts))
@@ -26,10 +34,14 @@ func main() {
 		os.Exit(0)
 		return
 	}
-	cmd := exec.Command("ssh", h)
-	cmd.Stdout = os.Stdout
-	cmd.Stdin = os.Stdin
-	cmd.Stderr = os.Stderr
-	cmd.Run()
 
+	if *showPtr {
+		pp.Print(m[h])
+	} else {
+		cmd := exec.Command("ssh", h)
+		cmd.Stdout = os.Stdout
+		cmd.Stdin = os.Stdin
+		cmd.Stderr = os.Stderr
+		cmd.Run()
+	}
 }
